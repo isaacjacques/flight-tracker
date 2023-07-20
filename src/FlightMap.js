e23288699ed1a86e1fe226aa8fb2c059722aba9d
isaacjacques <you@example.comname>
2023-07-19 22:45:45 -0400

    replace jsx syntax

--- src/FlightMap.js
+++ src/FlightMap.js
@@ -4,7 +4,7 @@ import { Marker, Popup } from "react-leaflet";
 import { useMapEvents } from 'react-leaflet/hooks'
 import 'leaflet/dist/leaflet.css';
 import { IoIosAirplane } from 'react-icons/io';
-import L from 'leaflet';
+import { divIcon } from 'leaflet';
 import axios from 'axios'; 
 //import testdata from './testdata.json';
 
@@ -52,33 +52,33 @@ export default function FlightMap() {
     }, [currentBounds, flights]);
 
 
-    const flightIcon = new L.divIcon({
+    const flightIcon = new divIcon({
         className: 'flight-icon',
-        html: ReactDOM.renderToString(<IoIosAirplane size="33" color = "#CF9603"/>),
-        
+        html: ReactDOM.renderToString(React.createElement(IoIosAirplane, { size: "33", color: "#CF9603" })),
     }); 
 
-
-    return(
-        <>
-            {flightsDisplayed.map((flight) => (
-                <Marker 
-                    key = {flight[0]} 
-                    position={[flight[6], flight[5]]} 
-                    icon={flightIcon}
-                >
-                    <Popup autoClose={true}>
-                        <div>
-                            <p><strong>ICAO24: </strong>{flight[0]}</p>
-                            <p><strong>Callsign: </strong>{flight[1]}</p>
-                            <p><strong>Origin: </strong>{flight[2]}</p>
-                            <p><strong>Position: </strong>{flight[6]}, {flight[5]}</p>
-                            <p><strong>Altitude: </strong>{flight[7] ? flight[7] : 0}</p>
-                            <p><strong>Velocity: </strong>{flight[9]}</p>
-                        </div>
-                    </Popup>
-                </Marker>
-            ))}
-        </>
-    );
+    return React.createElement(React.Fragment, null,
+        flightsDisplayed.map((flight) => {
+          const key = flight[0];
+          const position = [flight[6], flight[5]];
+          const popupContent = (
+            React.createElement('div', null,
+              React.createElement('p', null, React.createElement('strong', null, 'ICAO24: '), flight[0]),
+              React.createElement('p', null, React.createElement('strong', null, 'Callsign: '), flight[1]),
+              React.createElement('p', null, React.createElement('strong', null, 'Origin: '), flight[2]),
+              React.createElement('p', null, React.createElement('strong', null, 'Position: '), `${flight[6]}, ${flight[5]}`),
+              React.createElement('p', null, React.createElement('strong', null, 'Altitude: '), flight[7] ? flight[7] : 0),
+              React.createElement('p', null, React.createElement('strong', null, 'Velocity: '), flight[9])
+            )
+          );
+    
+          return React.createElement(Marker, {
+            key: key,
+            position: position,
+            icon: flightIcon
+          },
+            React.createElement(Popup, { autoClose: true }, popupContent)
+          );
+        })
+      );
 }
--- src/FlightMapContainer.js
+++ src/FlightMapContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import FlightMap from './FlightMap';
@@ -10,22 +10,21 @@ export default function FlightMapContainer() {
     ];
    
 
-    return (
-        <MapContainer 
-            center={[28,-81]} 
-            zoom={5} 
-            minZoom={3} 
-            scrollWheelZoom={false}
-            style={{ width: '100%', height: '500px' }} 
-            noWrap={true} 
-            maxBounds={mapBounds} 
-            maxBoundsViscosity={1.0}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <FlightMap />
-        </MapContainer>
-    );
+    return React.createElement(MapContainer, {
+        center: [28, -81],
+        zoom: 5,
+        minZoom: 3,
+        scrollWheelZoom: false,
+        style: { width: '100%', height: '500px' },
+        noWrap: true,
+        maxBounds: mapBounds,
+        maxBoundsViscosity: 1.0
+      }, [
+        React.createElement(TileLayer, {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          key: 'tileLayer'
+        }),
+        React.createElement(FlightMap, { key: 'flightMap' }) // Assuming you have defined the FlightMap component
+      ]);
 }
